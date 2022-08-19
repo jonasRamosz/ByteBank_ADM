90b41fc6a7f458492804a1aae46ae88b292590e6
jonasRamosz <jrenovaveis@gmail.com>
2022-08-18 22:50:00 -0300

    primeiro commit

---
+++ bytebank_ADM/Gerencia/GerenciadorDeBonificacao.cs
@@ -0,0 +1,24 @@
+﻿using bytebank_ADM.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Gerencia
+{
+    public class GerenciadorDeBonificacao
+    {
+        private double totalBonificacao;
+
+        public void Registra(Funcionario funcionario)
+        {
+            this.totalBonificacao += funcionario.getBonificacao();
+        }
+
+        public double getBonificacao()
+        {
+            return this.totalBonificacao;
+        }
+    }
+}
---
+++ bytebank_ADM/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using bytebank_ADM.Funcionarios;
+using bytebank_ADM.Gerencia;
+
+Console.WriteLine("Boas vindas, ao ByteBank Administração");
+
+Funcionario jonas = new Funcionario();
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+jonas.Nome = "jonas";
+jonas.Salario = 1000;
+Console.WriteLine(jonas.Nome);
+Console.WriteLine(jonas.getBonificacao()+jonas.Salario);
+
+Funcionario hadassa = new Diretor();
+hadassa.Nome = "hadassa";
+hadassa.Salario = 2000;
+Console.WriteLine(hadassa.Nome);
+Console.WriteLine(hadassa.getBonificacao());
+
+gerenciador.Registra(hadassa);
+gerenciador.Registra(jonas);
+Console.WriteLine(gerenciador.getBonificacao());
